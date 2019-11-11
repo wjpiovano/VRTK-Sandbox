20d8c719ce76f3ea1708612b2f22077fcc949cac
piuvans <wjpiovano@hotmail.com>
2019-11-11 11:40:29 +0000

    Updated core files.

---
+++ VRTK Sandbox/Assets/_Scripts/PointingGestureAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zinnia.Action;
+
+public class PointingGestureAction : BooleanAction
+{
+  public bool gripTrigger { get; set; }
+  public bool indexTrigger { get; set; }
+  public bool isGrabbing { get; set; }
+
+  void Update()
+  {
+    Receive(gripTrigger && !indexTrigger && !isGrabbing);
+  }
+}
---
+++ VRTK Sandbox/Assets/_Scripts/Utils/OVRAxis1DAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zinnia.Action;
+
+public class OVRAxis1DAction : FloatAction
+{
+  public OVRInput.Controller controller = OVRInput.Controller.Active;
+  public OVRInput.Axis1D axis;
+
+  void Update()
+  {
+    Receive(OVRInput.Get(axis, controller));
+  }
+}
