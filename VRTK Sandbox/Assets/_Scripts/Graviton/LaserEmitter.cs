9134e1c87510ed38c74ba123ff49470afe2d8d39
piuvans <wjpiovano@hotmail.com>
2019-12-10 01:56:52 +0000

    Laser emitter. Laser receptor first cut.

---
+++ VRTK Sandbox/Assets/_Scripts/Graviton/LaserEmitter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEmitter : MonoBehaviour
+{
+  private LineRenderer lineRenderer;
+
+  private void Start()
+  {
+    lineRenderer = GetComponent<LineRenderer>();
+  }
+
+  private void LateUpdate()
+  {
+    lineRenderer.SetPosition(0, transform.position);
+    RaycastHit hit;
+    if (Physics.Raycast(transform.position, transform.up, out hit))
+    {
+      if (hit.collider != null)
+      {
+        lineRenderer.SetPosition(1, hit.point);
+      }
+    }
+    else
+    {
+      lineRenderer.SetPosition(1, transform.position + transform.up * 10);
+    }
+  }
+}
